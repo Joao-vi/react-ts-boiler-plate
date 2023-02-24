e087e6be702889bff167b81a59303d9c5ac6f58f
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-24 14:44:55 -0400

    chore: last changes

--- src/index.tsx
+++ src/index.tsx
@@ -1,7 +1,11 @@
-import React from 'react';
 import { createRoot } from 'react-dom/client';
 
+const App = () => {
+  return <h1 style={{ color: 'red' }}>Basic Taaemplate</h1>;
+};
 
 const container = document.getElementById('root');
+if (!container) throw new Error('The root Element was not found');
+
 const root = createRoot(container);
 root.render(<App />);
