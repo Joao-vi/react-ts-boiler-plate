ce66c1ca8dcb07c6a19f614ddeb800f74eb0a4da
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-24 14:38:50 -0400

    chore: add new dependencies

--- jest.config.js
+++ jest.config.js
@@ -1,14 +1,17 @@
 module.exports = {
+  testEnvironment: 'jsdom',
   moduleFileExtensions: ['ts', 'tsx', 'js', 'json'],
   moduleNameMapper: {
     '\\.scss$': 'identity-obj-proxy',
-    '^@components/(.*)$': '<rootDir>/src/components/$1',
+    '^@/(.*)$': '<rootDir>/src/$1',
   },
   collectCoverageFrom: ['<rootDir>/**/*.{ts, tsx}'],
-  roots: ['<rootDir>'],
+  roots: ['<rootDir>/src'],
   testRegex: '(/tests/jest/.*|(\\.|/)(test|spec))\\.(ts|tsx)$',
   transform: {
     '^.+\\.(ts|tsx)$': 'ts-jest',
     '^.+\\.svg$': '<rootDir>/utils/svgTransform.js',
   },
+
+  testPathIgnorePatterns: ['<rootDir>/node_modules/', '<rootDir>/tests/e2e/cypress'],
 };
