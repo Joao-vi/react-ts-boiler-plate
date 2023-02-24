ea0ec2fc4b814e34d11eb471400f44832b4d2a1b
joao-vi <joaovictor.go.ro@gmail.com>
2023-02-24 11:10:50 -0400

    chore: remove unused files

--- src/index.tsx
+++ src/index.tsx
@@ -1,8 +1,6 @@
 import React from 'react';
 import { createRoot } from 'react-dom/client';
 
-import App from './components/App/index';
-import './index.scss';
 
 const container = document.getElementById('root');
 const root = createRoot(container);
